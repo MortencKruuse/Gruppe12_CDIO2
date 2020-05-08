f8ab9c040b96d8b117710d843a5f3ab5a9d20e65
MortencKruuse <s183681@student.dtu.dk>
2020-05-08 11:56:19 +0200

    Update git Ignore

--- src/main/java/api/PersonService.java
+++ src/main/java/api/PersonService.java
@@ -5,18 +5,18 @@ import data.DTO.UserDTO;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-
-@Path("person")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Path("person")
 public class PersonService {
     private static PersonController personController = new PersonController();
     @GET
     public String welcome(){
         return "Hello user";
     }
+
     @PUT
-    public void createUser(@PathParam("name") String name){
+    public void createUser(@PathParam("uname") String uname){
 
     }
 
