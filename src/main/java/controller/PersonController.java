806ce261e1aa20fa4ca10614f6417aa7a49d18b7
Sejr <sejrlba@gmail.com>
2020-05-10 16:21:33 +0200

    Deletede bubbers and edited index so fields could not be NULL.

--- src/main/java/controller/PersonController.java
+++ src/main/java/controller/PersonController.java
@@ -10,16 +10,6 @@ public class PersonController {
 
     public PersonController() {
         localStorage = new LocalStorage();
-        localStorage.createUser("Bubbers1", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers2", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers3", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers4", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers5", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers6", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers7", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers8", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers9", "Boob", "12345678", "iLikBoobs","Admin");
-        localStorage.createUser("Bubbers10", "Boob", "12345678", "iLikBoobs","Admin");
     }
 
     public void createUser(String name, String ini, String cpr, String password, String role) {
